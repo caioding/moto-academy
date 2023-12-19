3ad8949600d44301a5fb869dfb74bc9b43978470
Caio Cesar <7caioc@gmail.com>
2023-12-18 20:15:26 -0400

    requisições com postman

--- node/index.js
+++ node/index.js
@@ -8,11 +8,49 @@
 // moduloA.valorModuloA()
 
 const express = require('express')
-
 const app = express()
 
-app.get("/", function(req, res){
-    res.send("Hello World")
+app.use(express.json())
+
+let users = [
+    { id: 1, name: "Caio" },
+    { id: 2, name: "Daio" },
+    { id: 3, name: "Eaio"},
+]
+
+app.get("/", function (req, res) {
+    res.send(users)
+})
+
+app.get("/", function(req,res){
+    res.send(users)
+})
+
+app.get("/:id", function(req,res){
+    const id = parseInt(req.params.id)
+    const user = users.find(user => user.id === id)
+    if(user) {
+        res.json(user)
+    }
+    else{
+        res.status(400).json({message: "Usuário não encontrado"})
+    }
+})
+
+app.post("/users", function (req, res) {
+    console.log(req.body);
+    const { name } = req.body
+    const id = users.length + 1
+    const newUser = { id, name }
+
+    users.push(newUser)
+    res.status(201).json(newUser)
+})
+
+app.delete("/users/:id", (req,res) => {
+    const id = parseInt(req.params.id)
+    users = users.filter(user => user.id !== id);
+    res.sendStatus(204)
 })
 
 app.listen(3000)
