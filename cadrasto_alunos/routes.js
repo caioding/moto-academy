1af0a7371a0c022c45aedd7df205f11d1546733f
Caio Cesar <7caioc@gmail.com>
2024-01-12 20:06:53 -0400

    CRUD de alunos e aulas

--- cadrasto_alunos/routes.js
+++ cadrasto_alunos/routes.js
@@ -15,18 +15,79 @@ router.get("/alunos", (req, res) => {
     });
 });
 
-router.post('/alunos', (req,res) =>{
-    const{nome, idade} = req.body;
+router.post('/alunos', (req, res) => {
+    const { nome, idade } = req.body;
 
     const query = `INSERT INTO alunos(nome, idade)  VALUES ('${nome}', ${idade})`;
 
-    db.query(query, (err, result) =>{
-        if(err){
+    db.query(query, (err, result) => {
+        if (err) {
             res.status(500).send('Erro ao criar aluno');
-        }else{
+        } else {
             res.status(201).send('Aluno criado com sucesso');
         }
     });
 });
+
+//Endpoint de atualização de aluno (update)
+router.put('/alunos/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    const { nome, idade } = req.body;
+
+    const query = `UPDATE alunos SET nome='${nome}', idade= ${idade} WHERE id_aluno= ${id}`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).send('Erro ao atualizar aluno');
+        } else {
+            res.send('Aluno atualizado com sucesso');
+        }
+    });
+});
+
+// Endpoint de exclusão aluno (delete
+router.delete('/alunos/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    const query = `DELETE FROM alunos WHERE id_aluno= ${id}`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).send('Erro ao excluir aluno');
+        } else {
+            res.send('Aluno excluído com sucesso');
+        }
+    });
+});
+
+router.post("/aulas", (req,res) =>{
+    const {nome_aula, descricao} = req.body;
+
+    const query = `INSERT INTO aulas(nome_aula, descricao) VALUES('${nome_aula}', '${descricao}')`;
+
+    db.query(query, (err, result) =>{
+        if(err){
+            res.status(500).send('Erro ao  criar aula');
+        }else{
+            res.status(201).send('Aula criado com sucesso');
+        }
+    });
+})
+
+router.get("/aulas", (req,res) =>{
+
+    const query = `SELECT * FROM aulas`;
+
+    db.query(query, (err, result) =>{
+        if(err){
+            res.status(500).send(`Erro ao obter aulas: ${JSON.stringify(err)}` );
+        }else{
+            res.json(result )
+        }
+    });
+})
 
 module.exports = router;
